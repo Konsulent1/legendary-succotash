af0c707b3ff590e0ba41991073fdffcfa4b501cb
oystein <oebars@gmail.com>
2017-04-19 13:50:49 +0200

    added classes

---
+++ TMS/src/tms/Department.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tms;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by øystein
+ */
+@Data
+@NoArgsConstructor
+class Department {
+    
+    private int departmentID;
+    private String name;
+    
+}
---
+++ TMS/src/tms/Orderline.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tms;
+
+/**
+ *
+ * @author oebar
+ */
+public class Orderline {
+    
+    private int orderID;
+    private int productID;
+    private int quantity;
+    
+    
+}
